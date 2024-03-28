df3afa96c5017ad5f44755a5d775a233de6e85be
starchaser777 <les980730@gmail.com>
2024-03-28 11:52:01 +0900

    Initial commit

---
+++ src/test/java/kr/ac/kopo/webapplication/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
