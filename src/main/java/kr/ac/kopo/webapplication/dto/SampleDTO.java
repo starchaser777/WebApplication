4f829d850cc1affee313c6460f44745fa5062c6b
starchaser777 <les980730@gmail.com>
2024-04-04 11:54:57 +0900

    Thymeleaf - SampleController,SampleDTO

---
+++ src/main/java/kr/ac/kopo/webapplication/dto/SampleDTO.java
@@ -0,0 +1,15 @@
+package kr.ac.kopo.webapplication.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+}
